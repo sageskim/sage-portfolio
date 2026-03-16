29d310b789e03424d9e403131e3e06b829d599e1
Sage Kim <seyeonk4@illinois.edu>
2026-03-16 17:42:42 -0500

    Add About page with personal information

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <section className="page">
+      <h1>About</h1>
+
+      <p>
+        I am an Information Science + Data Science student at UIUC.
+      </p>
+
+      <p>
+        My interests lie in the intersection of data, design, and media.
+      </p>
+    </section>
+  )
+}
