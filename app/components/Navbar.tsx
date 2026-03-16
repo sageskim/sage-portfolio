7215b013f0dfef5f870a9b6f683865951c4ae766
Sage Kim <seyeonk4@illinois.edu>
2026-03-16 17:39:39 -0500

    Add Navbar component with navigation links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,1 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <div className="logo">Sage Kim</div>
 
+      <div className="menu">
+        <a href="/about">About</a>
+        <a href="/projects">Projects</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </nav>
+  )
+}
