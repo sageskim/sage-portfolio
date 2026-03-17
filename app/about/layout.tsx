5b3d201c6ea4e0735bd4044a70c7dd4eab9b0e9c
Sage Kim <seyeonk4@illinois.edu>
2026-03-17 01:16:40 -0500

    Create AboutLayout component with background style

---
+++ app/about/layout.tsx
@@ -0,0 +1,8 @@
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div style={{ background: "#BCEDFF", minHeight: "100vh" }}>{children}</div>;
+}
+ 
