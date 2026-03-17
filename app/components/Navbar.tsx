036d777782a33fc00c130e028da73a18fe3c3041
Sage Kim <seyeonk4@illinois.edu>
2026-03-16 21:38:05 -0500

    Refactor Navbar component structure

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,15 @@
 export default function Navbar() {
   return (
     <nav className="nav">
-      <div className="logo">Sage Kim</div>
 
-      <div className="menu">
+      <div>Sage Kim</div>
+
+      <div className="links">
         <a href="/about">About</a>
         <a href="/projects">Projects</a>
         <a href="/contact">Contact</a>
       </div>
+
     </nav>
   )
 }
